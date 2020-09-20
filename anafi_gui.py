5fb522bada17b431e25677f8084a7a36a7d92421
Franklin Boampong <fboamps@gmail.com>
2020-09-19 23:01:54 -0400

    Added code for tkinter gui

--- anafi_gui.py
+++ anafi_gui.py
@@ -10,33 +10,18 @@ import cv2
 import os
 
 def videoLoop(self):
-		# try/except statement is a pretty ugly hack to get around
-		# a RunTime error that Tkinter throws due to threading
-		try:
-			# keep looping over frames until we are instructed to stop
-			while not self.stopEvent.is_set():
-				# grab the frame from the video stream and resize it to
-				# have a maximum width of 300 pixels
-				self.frame = self.vs.read()
-				self.frame = imutils.resize(self.frame, width=300)
-		
-				# OpenCV represents images in BGR order; however PIL
-				# represents images in RGB order, so we need to swap
-				# the channels, then convert to PIL and ImageTk format
-				image = cv2.cvtColor(self.frame, cv2.COLOR_BGR2RGB)
-				image = Image.fromarray(image)
-				image = ImageTk.PhotoImage(image)
-		
-				# if the panel is not None, we need to initialize it
-				if self.panel is None:
-					self.panel = tki.Label(image=image)
-					self.panel.image = image
-					self.panel.pack(side="left", padx=10, pady=10)
-		
-				# otherwise, simply update the panel
-				else:
-					self.panel.configure(image=image)
-					self.panel.image = image
+	try:
+		while not self.stopEvent.is_set():
+			self.frame = self.vs.read()
+			self.frame = imutils.resize(self.frame, width=300)
+
+			if self.panel is None:
+				self.panel = tki.Label(image=image)
+				self.panel.image = image
+				self.panel.pack(side="left", padx=10, pady=10)
+			else:
+				self.panel.configure(image=image)
+				self.panel.image = image
 		except ValueError:
 			print("Caught a threading RuntimeError")
 
---
+++ control_gui.py
@@ -0,0 +1,80 @@
+import tkinter as tk
+from tkinter import *
+from PIL import Image
+from PIL import ImageTk
+
+HEIGHT = 600
+WIDTH = 800
+BUTTON_WIDTH = 50
+BUTTON_HEIGHT= 50
+ROTATE_BUTTON_WIDTH = 70
+ROTATE_BUTTON_HEIGHT = 400
+
+def sel():
+   selection = "Value = " + str(var.get())
+   label.config(text = selection)
+
+# setting up screen 
+root = tk.Tk()
+root.resizable(False, False)
+root.title("Anafi Drone GUI")
+
+canvas = tk.Canvas(root, height=HEIGHT, width=WIDTH)
+canvas.pack()
+
+title = tk.Label(canvas, text="Flight Controller v1", font=('Century', 20), anchor='center')
+title.place(relx=0.15, rely=0.02, relwidth=0.7, relheight = 0.1)
+
+controlFrame=tk.Frame(root)
+controlFrame.place(relwidth=0.9, relheight=0.7, relx=0.05, rely=0.15)
+
+# setting up the slider
+var = DoubleVar()
+scale = Scale(controlFrame, variable = var, from_= 50, to=-50)
+scale.place(relwidth=0.2, relheight=0.9, relx=0.3, rely=0.05)
+
+# left button 
+left_button_image = Image.open("Left_button.png")
+left_img = left_button_image.resize((BUTTON_WIDTH, BUTTON_HEIGHT), Image.ANTIALIAS)
+left_photoImg =  ImageTk.PhotoImage(left_img)
+leftbutton=Button(controlFrame, image = left_photoImg)
+leftbutton.place(relwidth=0.075, relheight=0.1, relx = 0.5, rely=0.4)
+
+#right button
+right_button_image = Image.open("Right_button.png")
+right_img = right_button_image.resize((BUTTON_WIDTH, BUTTON_HEIGHT), Image.ANTIALIAS)
+right_photoImg =  ImageTk.PhotoImage(right_img)
+rightbutton=Button(controlFrame, image = right_photoImg)
+rightbutton.place(relwidth=0.075, relheight=0.1, relx = 0.7, rely=0.4)
+
+#top button
+top_button_image = Image.open("Up_button.png")
+top_img = top_button_image.resize((BUTTON_WIDTH, BUTTON_HEIGHT), Image.ANTIALIAS)
+top_photoImg =  ImageTk.PhotoImage(top_img)
+topbutton=Button(controlFrame, image = top_photoImg)
+topbutton.place(relwidth=0.075, relheight=0.1, relx = 0.6, rely=0.2)
+
+#down button
+down_button_image = Image.open("Down_button.png")
+down_img = down_button_image.resize((BUTTON_WIDTH, BUTTON_HEIGHT), Image.ANTIALIAS)
+down_photoImg =  ImageTk.PhotoImage(down_img)
+downbutton=Button(controlFrame, image = down_photoImg)
+downbutton.place(relwidth=0.075, relheight=0.1, relx = 0.6, rely=0.6)
+
+# rotate left 
+l_rotate_button_image = Image.open("Left_Rotate.png")
+l_rotate_img = l_rotate_button_image.resize((ROTATE_BUTTON_WIDTH, ROTATE_BUTTON_HEIGHT), Image.ANTIALIAS)
+l_rotate_photoImg =  ImageTk.PhotoImage(l_rotate_img)
+l_rotate_button=Button(controlFrame, image = l_rotate_photoImg)
+l_rotate_button.place(relwidth=0.1, relheight=0.8, relx = 0.1, rely=0.1)
+
+# rotate right 
+r_rotate_button_image = Image.open("Right_Rotate.png")
+r_rotate_img = r_rotate_button_image.resize((ROTATE_BUTTON_WIDTH, ROTATE_BUTTON_HEIGHT), Image.ANTIALIAS)
+r_rotate_photoImg =  ImageTk.PhotoImage(r_rotate_img)
+r_rotate_button=Button(controlFrame, image = r_rotate_photoImg)
+r_rotate_button.place(relwidth=0.1, relheight=0.8, relx = 0.8, rely=0.1)
+
+
+# Main Loop Start:
+root.mainloop()
